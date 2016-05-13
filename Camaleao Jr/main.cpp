425e071ec93c262d69c6fb79ea769707859cb0eb
Luan <luanccp@gmail.com>
2016-05-13 10:57:51 -0300

    Initial Commit

---
+++ Camaleao Jr/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Camaleao Jr
+//
+//  Created by Luan Castheloge on 5/13/16.
+//  Copyright (c) 2016 Luan Castheloge. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
