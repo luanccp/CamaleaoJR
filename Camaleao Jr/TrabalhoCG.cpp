bf1a922470d4146b25479d9c4a389daf1734bdfc
filipe.canicali <filipe.canicali@gmail.com>
2016-05-18 20:54:49 -0300

    movimentos de perna lingua e deslocamento do camaleão

--- Camaleao Jr/TrabalhoCG.cpp
+++ Camaleao Jr/TrabalhoCG.cpp
@@ -6,18 +6,10 @@
 using namespace std;
 int dx,dy;
 int theta;
-
-void DesenhaRetangulo(float largura, float altura, float r,float g, float b)
-{
-	/*glBegin(GL_QUADS);
-		glColor3f(r, g, b);	
-		glVertex2f(-largura/2,0.0);
-		glVertex2f(largura,0.0);
-		glVertex2f(largura,altura);
-		glVertex2f(0.0,altura);
-	glEnd();*/
-}
-
+int contecla = 0;
+int lingua = 0;
+float g = 0;
+int vetor[256]= {0};
 
 void drawHollowCircle(float x, float y, float radius)
 {
@@ -39,12 +31,8 @@ void drawHollowCircle(float x, float y, float radius)
 	glPopMatrix();
 }
 
-void DesenhaRabo(void)
+void desenhaRabo(void)
 {
-
-	//drawHollowCircle(0.0, 0.0, 20.0);
-		
-
 	glColor3f(0.0f, 0.0f, 1.0f); // azul
 	
    glEnd();
@@ -79,7 +67,7 @@ void DesenhaRabo(void)
 	glEnd();
 
 }
-void DesenhaCorpo1()
+void desenhaCorpo1()
 {
 	glColor3f(0.0f, 0.0f, 1.0f); // azul
 	glBegin(GL_POLYGON);
@@ -92,7 +80,7 @@ void DesenhaCorpo1()
 	glEnd();	
 
 }
-void DesenhaCorpo2()
+void desenhaCorpo2()
 {
 	glColor3f(0.0f, 0.0f, 1.0f); // azul
 	glBegin(GL_POLYGON);
@@ -103,7 +91,7 @@ void DesenhaCorpo2()
 		glVertex3f(-15.0,15.0,0.0);
 	glEnd();
 }
-void DesenhaCabeca1()
+void desenhaCabeca1()
 {
 	glColor3f(0.0f, 0.0f, 1.0f); // azul
 	glBegin(GL_TRIANGLE_FAN);
@@ -120,16 +108,8 @@ void DesenhaCabeca1()
 	glEnd();
 
 }
-void DesenhaCabeca2()
+void desenhaCabeca2()
 {
-	//LINGUA
-	glColor3f(1.0f, 0.0f, 0.0f); //VERMELHO
-	glBegin(GL_LINES);
-		glVertex3f(-2.0,-7.0,0.0);
-		glVertex3f(-20.0,-7.0,0.0);
-		
-	glEnd();	
-
 	glColor3f(0.0f, 0.0f, 1.0f); // azul
 	glBegin(GL_TRIANGLE_FAN);
 		glVertex3f(0.0,-5.0,0.0);
@@ -141,14 +121,14 @@ void DesenhaCabeca2()
 		      
 	glEnd();
 }
-void DesenhaPe1()
+void desenhaPe1()
 {
 	glColor3f(0.0f, 0.0f, 1.0f); // azul
 	glBegin(GL_QUADS);
-		glVertex3f(0.0,0.0,0.0);
+		glVertex3f(0.0,5.0,0.0);
 		glVertex3f(2.0,-5.0,0.0);
 		glVertex3f(7.0,-5.0,0.0);
-		glVertex3f(5.0,-0.0,0.0);
+		glVertex3f(5.0,5.0,0.0);
 	glEnd();
 	glBegin(GL_QUADS);
 		glVertex3f(2.0,-5.0,0.0);
@@ -161,38 +141,33 @@ void DesenhaPe1()
 		glVertex3f(10.0,-10.0,0.0);
 	glEnd();
 }
-void DesenhaPe2()
+void desenhaPe2()
 {
 	glColor3f(0.0f, 0.0f, 1.0f); // azul
 	glBegin(GL_QUADS);
-		glVertex3f(0.0,0.0,0.0);
+		glVertex3f(0.0,5.0,0.0);
 		glVertex3f(0.0,-10.0,0.0);
 		glVertex3f(5.0,-10.0,0.0);
-		glVertex3f(5.0,0.0,0.0);
+		glVertex3f(5.0,5.0,0.0);
 	glEnd();
 	glBegin(GL_LINES);
 		glVertex3f(-5.0,-10.0,0.0);	
 		glVertex3f(10.0,-10.0,0.0);
 	glEnd();
 }
-void olho()
+void desenhaOlho()
 {
 	glColor3f( 0.0, 0.0, 0.0 );
 	drawHollowCircle(0,0,2);
 	drawHollowCircle(0,0,4);
 }
 
-void desenhaQuadrado (float largura, float altura, float r,float g, float b)
-{
-	/*//desenhaQuadrado(100.0,40.0,0.0,1.0,1.0,1.0)
-	glBegin(GL_QUADS);
-		glColor3f(r, g, b);
-		glVertex2f( -largura/2, 0);
-		glVertex2f( largura/2, 0);
-		glVertex2f( largura/2, altura);
-		glVertex2f( -largura/2, altura);
-	glEnd();
-	  */  
+void desenhaLingua(){
+	glColor3f(1.0f, 0.0f, 0.0f); //VERMELHO
+	glBegin(GL_LINES);
+		glVertex3f(-2.0,-7.0,0.0);
+		glVertex3f(g,-7.0,0.0);
+	glEnd();	
 }
 
 void display( void )
@@ -200,91 +175,75 @@ void display( void )
     //Limpar todos os pixels
     glClear( GL_COLOR_BUFFER_BIT );
     
-    
-    glColor3f( 0.0, 0.0, 0.0 );
-    glBegin(GL_LINES);
-		glVertex2f(400, 0);
-		glVertex2f(-400, 0);
-	glEnd();
-	glColor3f( 0.0, 1.0, 1.0 );
-    glBegin(GL_LINES);
-		glVertex2f(0, -400);
-		glVertex2f(0, 400);
-	glEnd();
-	
-	glPushMatrix();
-		
-		glTranslatef(dx, dy, 0);	
-		desenhaQuadrado(100.0,40.0,0.0,1.0,1.0);	
-	glPopMatrix();	
-		
-	glPushMatrix();
-		glTranslatef(dx-40, dy, 0);		
-		glRotatef(-dx,0,0,1);		
-		glColor3f(0.0,0.0,0.0);
-		//drawHollowCircle(0,0,15);	
-	glPopMatrix();
-
-	glPushMatrix();
-		glTranslatef(dx+40, dy, 0);		
-		glColor3f(0.0,0.0,0.0);
-		glRotatef(-dx,0,0,1);
-		//drawHollowCircle(0,0,15);	
-	glPopMatrix();
-	
-	glPushMatrix();
-		glTranslatef(dx-25, dy+40, 0);		
-		glRotatef(-theta,0,0,1);
-		desenhaQuadrado(10.0,80.0,0.0,0.0,0.0);	
-	glPopMatrix();
-
-		glPushMatrix();
-		glTranslatef(dx, dy+40, 0);		
-		glRotatef(-theta,0,0,1);
-		desenhaQuadrado(10.0,80.0,0.0,0.0,0.0);
-	glPopMatrix();
-
-		glPushMatrix();
-		glTranslatef(dx+25, dy+40, 0);		
-		glRotatef(-theta,0,0,1);
-		desenhaQuadrado(10.0,80.0,0.0,0.0,0.0);	
-	glPopMatrix();
-
 	glPushMatrix();	
 		glTranslatef(dx+70, dy+11, 0);
-		DesenhaRabo();	
+		desenhaRabo();	
 	glPopMatrix();
 	
 	glPushMatrix();
 		glTranslatef(dx+60, dy, 0);
-		DesenhaCorpo1();
+		desenhaCorpo1();
 	glPopMatrix();
 	glPushMatrix();
 		glTranslatef(dx+45, dy, 0);
-		DesenhaCorpo2();
+		desenhaCorpo2();
+	glPopMatrix();
+
+	if(lingua == 1){	
+		glPushMatrix();
+			glTranslatef(dx+25, dy, 0);
+			desenhaLingua();
+		glPopMatrix();
+	}
+
+	glPushMatrix();
+		glTranslatef(dx+25, dy, 0);
+		desenhaCabeca1();
 	glPopMatrix();
 	glPushMatrix();
 		glTranslatef(dx+25, dy, 0);
-		DesenhaCabeca1();
-	glPopMatrix();
-	glPushMatrix();
-		glTranslatef(dx+25, dy, 0);
-		DesenhaCabeca2();
+		desenhaCabeca2();
 	glPopMatrix();
 	glPushMatrix();
 		glTranslatef(dx+55, dy-15, 0);
-		DesenhaPe1();
+		glRotatef(-theta,0,0,1);
+		desenhaPe1();
 	glPopMatrix();
 	glPushMatrix();
 		glTranslatef(dx+35, dy-15, 0);
-		DesenhaPe2();
+		glRotatef(-theta,0,0,1);
+		desenhaPe2();
 	glPopMatrix();
+	
 	glPushMatrix();
 		glTranslatef(dx+25, dy+7, 0);
-		olho();
+		desenhaOlho();
 	glPopMatrix();
-
+	
 	glutSwapBuffers();
+}
+void keyboardup(unsigned char tecla, int x, int y)
+{
+	switch(tecla)
+	{
+		case 'w':
+			
+			vetor[(int)('w')] = 0;
+			break;
+		case 's':
+
+			vetor[(int)('s')] = 0;
+			break;
+		case 'a':
+
+			vetor[(int)('a')] = 0;
+			break;
+		case 'd':
+
+			vetor[(int)('d')] = 0;
+			break;
+			}
+	glutPostRedisplay();
 }
 
 void init( void )
@@ -297,7 +256,32 @@ void init( void )
     glLoadIdentity();
     glOrtho( -250, 250, -250, 250, 100, -100 );
 } 
- 
+void idle(){
+	static char direction = -2;
+	static GLdouble previousTime = 0;
+	GLdouble currentTime;
+	GLdouble timeDifference;
+	
+	currentTime  = glutGet(GLUT_ELAPSED_TIME);
+	timeDifference = currentTime - previousTime;
+	previousTime = currentTime;
+	
+	if(vetor['a'])
+		dx-=2;//(0.1*timeDifference);
+	if(vetor['w'])
+		dy+=2;//(0.1*timeDifference);
+	if(vetor['s'])
+		dy-=2;//(0.1*timeDifference);;
+	if(vetor['d'])
+		dx+=2;//(0.1*timeDifference);
+	//if(vetor[])
+	//if(dx>250)
+	//	dx=-350;	
+	//else
+	//	dx+=(0.4*timeDifference);
+	
+	glutPostRedisplay();
+} 
 
 void keyboard(unsigned char tecla, int x, int y)
 {
@@ -307,20 +291,42 @@ void keyboard(unsigned char tecla, int x, int y)
 			exit(0);
 			break;
 		case 'w':
-			dy+=2;
+			vetor[(int)('w')] = 1;
+			//dy+=2;
 			break;
 		case 's':
-			dy-=2;
+			vetor[(int)('s')] = 1;
+			//dy-=2;
 			break;
 		case 'a':
-			dx-=2;
+			vetor[(int)('a')] = 1;
+			//dx-=2;
+			contecla++;
+			if((contecla%2) == 0 ){
+				theta = 0;
+				}
+			if((contecla%2) == 1 ){
+				theta = -45;
+				}
 			break;
 		case 'd':
-			dx+=2;
+			vetor[(int)('d')] = 1;
+			//dx+=2;
+			contecla++;
+			if((contecla%2) == 0 ){
+				theta = 0;
+				}
+			if((contecla%2) == 1 ){
+				theta = 45;
+				}
+			break;			
+		case 'z':
+			lingua = 1;
+			g-=2;			
 			break;
-		case 'f':
-			theta+=2;
-			if(theta > 90){theta = 90;}				
+		case 'x':
+			lingua = 0;
+			g=-2;
 			break;
 		case 'e':
 			theta-=2;
@@ -342,8 +348,11 @@ int main(int argc, char *argv[])
     init();
     
     glutDisplayFunc( display );
+     
     glutKeyboardFunc( keyboard );
-    
+    glutKeyboardUpFunc( keyboardup );	
+    glutIdleFunc(idle);
+        
     glutMainLoop();
     return 0;
 }
