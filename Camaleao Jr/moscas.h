807cd1bc162b08bc20e9de8e2d48b3c887c6677f
filipe.canicali <filipe.canicali@gmail.com>
2016-05-22 17:29:33 -0300

    commit inicial da mosca

--- Camaleao Jr/moscas.h
+++ Camaleao Jr/moscas.h
@@ -1,13 +1,64 @@
-//
-//  moscas.h
-//  Camaleao Jr
-//
-//  Created by Luan Castheloge on 5/22/16.
-//  Copyright (c) 2016 Luan Castheloge. All rights reserved.
-//
+#if defined __APPLE__
+#include <OpenGL/gl.h>
+#include <OpenGL/glu.h>
+#include <GLUT/glut.h>
+#else
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <GL/glut.h>
+#endif
+
+#include <iostream>
+#include <math.h>
+#include <stdio.h>
 
 #ifndef Camaleao_Jr_moscas_h
 #define Camaleao_Jr_moscas_h
 
 
 #endif
+
+
+void mosca_desenhaCorpo()
+{
+
+	glBegin(GL_POLYGON);
+		glVertex3f(-5,0.0,0.0);
+		glVertex3f(-8.0,-1.0,0.0);
+		glVertex3f(-10.0,-2.0,0.0);
+		glVertex3f(-9.0,-3.0,0.0);
+		glVertex3f(-7.0,-4.0,0.0);
+		glVertex3f(-0.0,-5.0,0.0);
+		glVertex3f(7.0,-4.0,0.0);
+		glVertex3f(9.0,-3.0,0.0);
+		glVertex3f(10.0,-2.0,0.0);
+		glVertex3f(8.0,-1.0,0.0);
+		glVertex3f(5,0.0,0.0);
+	glEnd();
+	
+}
+void mosca_desenhaAsas()
+{
+	glBegin(GL_POLYGON);
+		glVertex3f(-5.0,0.0,0.0);
+		glVertex3f(-10.0,-5.0,0.0);
+		glVertex3f(-8.0,-6.0,0.0);
+		glVertex3f(-6.0,-7.0,0.0);
+		glVertex3f(-4.0,-8.0,0.0);
+		glVertex3f(-2.0,-7.0,0.0);		
+		glVertex3f(-1.0,-6.0,0.0);		
+		glVertex3f(-0.0,-5.0,0.0);	
+	glEnd();
+}
+
+void mosca_display(void)
+{
+	glPushMatrix();
+	    glTranslatef(dx-70, dy-11, 0);
+	    mosca_desenhaCorpo();
+	glPopMatrix();	
+	glPushMatrix();
+	    glTranslatef(dx-70, dy-11, 0);
+	    mosca_desenhaAsas();
+	glPopMatrix();
+}
